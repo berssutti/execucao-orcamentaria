b2911bc046841149c4ea27a9b8cd22ae4cc690fa
Bernardo Pissutti <bernardopissutti@gmail.com>
2025-01-01 23:06:32 -0300

    Add requests to API Now it is possible to create projects with areas

--- frontend/src/components/ProjectForm.vue
+++ frontend/src/components/ProjectForm.vue
@@ -1,166 +1,212 @@
 <template>
-    <v-container>
-      <v-card elevation="2" class="pa-4">
-        <v-card-title>
-          {{ isEditing ? 'Editar Projeto' : 'Novo Projeto' }}
-        </v-card-title>
-        <v-divider></v-divider>
-  
-        <v-card-text>
-          <v-form @submit.prevent="saveProject">
-  
+  <v-container>
+    <v-card elevation="2" class="pa-4">
+      <v-card-title class="text-h5 font-weight-bold">
+        {{ isEditing ? 'Editar Projeto' : 'Novo Projeto' }}
+      </v-card-title>
+      <v-divider></v-divider>
+
+      <v-card-text>
+        <v-form @submit.prevent="saveProject">
+
+          <h4 class="mb-3">Detalhes Gerais</h4>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.name"
+                label="Nome do Projeto"
+                :rules="[rules.required, rules.maxLength(200)]"
+                placeholder="Digite o nome do projeto"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.description"
+                label="Descrição"
+                :rules="[rules.required, rules.maxLength(200)]"
+                placeholder="Descreva brevemente o projeto"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <h4 class="mb-3">Período</h4>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.start_date"
+                label="Data de Início"
+                type="date"
+                :rules="[rules.required]"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.end_date"
+                label="Data de Término"
+                type="date"
+                :rules="[rules.required]"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <h4 class="mb-3">Gerência</h4>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="project.coordinator"
+                label="Coordenador"
+                :rules="[rules.required, rules.maxLength(100)]"
+                placeholder="Nome do coordenador"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="project.substitute_coordinator"
+                label="Coordenador Substituto"
+                :rules="[rules.required, rules.maxLength(100)]"
+                placeholder="Nome do coordenador substituto"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="project.academic_supervisor"
+                label="Supervisor Acadêmico"
+                :rules="[rules.required, rules.maxLength(100)]"
+                placeholder="Nome do supervisor acadêmico"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <h4 class="mb-3">Orçamento</h4>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.total_unb_amount_expected"
+                label="Custos Indiretos UnB"
+                type="number"
+                :rules="[rules.required, rules.positive]"
+                placeholder="Valor esperado para UnB"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.total_fcte_amount_expected"
+                label="Custos Indiretos FCTE"
+                type="number"
+                :rules="[rules.required, rules.positive]"
+                placeholder="Valor esperado para FCTE"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <h4 class="mb-3">Status</h4>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="project.processo_sei"
+                label="Processo SEI"
+                placeholder="12345.123456/2024-12"
+                :rules="[rules.required, rules.seiFormat]"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-select
+                v-model="project.status"
+                :items="statusOptions"
+                label="Status"
+                @change="validateStatus"
+              ></v-select>
+            </v-col>
+          </v-row>
+
+          <div v-if="project.status === 'Recebido'">
+            <h4 class="mb-3">Célula Orçamentária</h4>
             <v-row>
-              <v-col cols="12" md="6">
+              <v-col cols="12" md="4">
                 <v-text-field
-                  v-model="project.name"
-                  label="Nome do Projeto"
-                  :rules="[rules.required, rules.maxLength(200)]"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="6">
-                <v-text-field 
-                  v-model="project.description"
-                  label="Descrição"
-                  :rules="[rules.required, rules.maxLength(200)]"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-  
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-text-field
-                  v-model="project.start_date"
-                  label="Data de Início"
-                  type="date"
+                  v-model="project.nota_dotacao"
+                  label="Nota de Dotação"
                   :rules="[rules.required]"
                   required
                 ></v-text-field>
               </v-col>
-              <v-col cols="12" md="6">
+              <v-col cols="12" md="4">
                 <v-text-field
-                  v-model="project.end_date"
-                  label="Data de Término"
-                  type="date"
+                  v-model="project.ptres"
+                  label="PTRES"
+                  :rules="[rules.required]"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  v-model="project.Ugr"
+                  label="UGR"
                   :rules="[rules.required]"
                   required
                 ></v-text-field>
               </v-col>
             </v-row>
-  
-            <v-row>
-              <v-col cols="12" md="4">
-                <v-text-field
-                  v-model="project.coordinator"
-                  label="Coordenador"
-                  :rules="[rules.required, rules.maxLength(100)]"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="4">
-                <v-text-field
-                  v-model="project.substitute_coordinator"
-                  label="Coordenador Substituto"
-                  :rules="[rules.required, rules.maxLength(100)]"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="4">
-                <v-text-field 
-                  v-model="project.academic_supervisor"
-                  label="Supervisor Acadêmico"
-                  :rules="[rules.required, rules.maxLength(100)]"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-  
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="project.total_unb_amount_expected"
-                  label="Custos Indiretos UnB"
-                  type="number"
-                  :rules="[rules.required, rules.positive]"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="project.total_fcte_amount_expected"
-                  label="Custos Indiretos FCTE"
-                  type="number"
-                  :rules="[rules.required, rules.positive]"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-  
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-text-field
-                  v-model="project.processo_sei"
-                  label="Processo SEI"
-                  :rules="[rules.required, rules.seiFormat]"
-                  placeholder="12345.123456/2024-12"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" md="6">
-                <v-select
-                  v-model="project.status"
-                  :items="statusOptions"
-                  label="Status"
-                  @change="validateStatus"
-                ></v-select>
-              </v-col>
-            </v-row>
-  
-            <div v-if="project.status === 'Received'">
-              <v-row>
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    v-model="project.nota_dotacao"
-                    label="Nota de Dotação"
-                    :rules="[rules.required]"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    v-model="project.ptres"
-                    label="PTRES"
-                    :rules="[rules.required]"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    v-model="project.Ugr"
-                    label="UGR"
-                    :rules="[rules.required]"
-                    required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </div>
-    
-            <v-row>
-              <v-col cols="12" class="text-right">
-                <v-btn type="submit" color="primary" class="mt-4">Salvar</v-btn>
-              </v-col>
-            </v-row>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-container>
-  </template>
-  
-  <script>
+          </div>
+
+          <h4 class="mb-3">Áreas do Projeto</h4>
+          <v-row v-for="(area, index) in project.areas" :key="index">
+            <v-col cols="6">
+              <v-select
+                v-model="area.id"
+                :items="filteredAreaList(index)"
+                item-title="name"
+                item-value="id"
+                label="Área"
+                :rules="[rules.required]"
+                required
+              ></v-select>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model.number="area.percentage"
+                label="Porcentagem"
+                type="number"
+                :rules="[rules.required, rules.percentage]"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="2" class="text-center">
+              <v-btn icon color="red" @click="removeArea(index)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-btn color="secondary" @click="addArea" class="mb-4">+ Adicionar Área</v-btn>
+
+          <v-divider class="my-4"></v-divider>
+
+          <v-row>
+            <v-col cols="12" class="text-right">
+              <v-btn color="secondary" class="mr-2" @click="$router.back()">Cancelar</v-btn>
+              <v-btn type="submit" color="primary">Salvar</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
   export default {
     name: 'ProjectForm',
-    props: ['id'], // Recebe o ID do projeto se estiver editando
+    props: ['id'],
     data() {
       return {
         project: {
@@ -172,7 +218,7 @@
           substitute_coordinator: '',
           academic_supervisor: '',
           processo_sei: '',
-          status: 'Processing', // Status padrão
+          status: 'Processando',
           nota_dotacao: '',
           ptres: '',
           Ugr: '',
@@ -180,8 +226,9 @@
           total_fcte_amount_expected: '',
           areas: [],
         },
+        selectedArea: null,
         areaList: [],
-        statusOptions: ['Processing', 'Received'],
+        statusOptions: ['Processando', 'Recebido'],
         isEditing: false,
         rules: {
           required: (value) => !!value || 'Campo obrigatório.',
@@ -192,7 +239,8 @@
           seiFormat: (value) =>
             /^\d{5}\.\d{6}\/\d{4}-\d{2}$/.test(value) || 
             'Formato inválido. Use: ddddd.dddddd/YYYY-MM',
-  
+          percentage: (value) =>
+            value >= 0 && value <= 100 || 'A porcentagem deve estar entre 0 e 100.',
         }
       };
     },
@@ -222,26 +270,46 @@
           console.error('Erro:', error);
         }
       },
+      filteredAreaList(index) {
+        const selectedAreas = this.project.areas
+          .map((a, i) => (i !== index ? a.id : null))
+          .filter((id) => id);
+        return this.areaList.filter((area) => !selectedAreas.includes(area.id));
+      },
+      addArea() {
+        this.project.areas.push({ name: '', percentage: 0 });
+      },
+      removeArea(index) {
+        this.project.areas.splice(index, 1);
+      },
+      validadePercentage() {
+        const totalPercentage = this.project.areas.reduce((sum, area) => sum + (Number(area.percentage) || 0), 0);
+        return totalPercentage === 100;
+      },
       validateStatus() {
         if (
-          this.project.status === 'Received' &&
+          this.project.status === 'Recebido' &&
           (!this.project.nota_dotacao || !this.project.ptres || !this.project.Ugr)
         ) {
           alert(
-            'Para alterar o status para "Received", é necessário preencher os campos Nota de Dotação, PTRES e UGR.'
+            'Para alterar o status para "Recebido", é necessário preencher os campos Nota de Dotação, PTRES e UGR.'
           );
-          this.project.status = 'Processing';
+          this.project.status = 'Processando';
         }
       },
       async saveProject() {
         try {
           if (
-            this.project.status === 'Received' &&
+            this.project.status === 'Recebido' &&
             (!this.project.nota_dotacao || !this.project.ptres || !this.project.Ugr)
           ) {
             alert(
-              'Para salvar com o status "Received", preencha os campos Nota de Dotação, PTRES e UGR.'
+              'Para salvar com o status "Recebido", preencha os campos Nota de Dotação, PTRES e UGR.'
             );
+            return;
+          }
+          if (!this.validadePercentage()) {
+            alert('A soma das porcentagens das áreas deve ser 100%');
             return;
           }
   
@@ -257,20 +325,58 @@
           });
   
           if (!response.ok) throw new Error('Erro ao salvar projeto');
-  
-          this.$router.push(`/projects/${this.isEditing ? this.id : (await response.json()).id}`);
+          
+          const projectData = await response.json();
+
+          await this.saveAreas(projectData.id);
+
+          this.$router.push(`/projects/${this.isEditing ? this.id : projectData.id}`);
         } catch (error) {
           console.error('Erro:', error);
         }
       },
+      async saveAreas(projectId) {
+        try {
+          for (const area of this.project.areas) {
+            if (area.id && area.percentage >= 0) {
+              const response = await fetch(`http://localhost:8000/api/projects/${projectId}/areas/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                  project: projectId,
+                  area: area.id,
+                  percentage: area.percentage,
+                }),
+              });
+              if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Erro ao salvar área:', errorData);
+                throw new Error('Erro ao salvar área');
+              }
+            } else {
+              console.error('Área ou porcentagem inválida:', area);
+            }
+          }
+        } catch (error) {
+          console.error('Erro ao salvar áreas:', error);
+          alert('Erro ao salvar áreas. Verifique os dados e tente novamente.');
+        }
+      },
     },
   };
-  </script>
+</script>
   
-  <style scoped>
+<style scoped>
   .v-card {
     max-width: 900px;
     margin: auto;
   }
-  </style>
+  h4 {
+    font-weight: bold;
+    margin-top: 16px;
+  }
+  .v-btn {
+    text-transform: none;
+  }
+</style>
   
