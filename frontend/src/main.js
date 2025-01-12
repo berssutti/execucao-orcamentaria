6b7089350bf0c43bb30b47dad5f8c5094631b8dc
Bernardo Pissutti <bernardopissutti@gmail.com>
2025-01-12 20:28:34 -0300

    Clean unused files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+import App from './App.vue';
+import router from './router';
+
 import '@mdi/font/css/materialdesignicons.css'
 
 
